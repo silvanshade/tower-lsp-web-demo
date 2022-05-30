5de13a7d19aaead2c29e1c0da60470ab941670ab
silvanshade <silvanshade@users.noreply.github.com>
2022-05-29 18:25:27 -0600

    Use AsyncIterator for input; works in all browsers

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,29 +1,55 @@
 class LspStdin {
-  static create(stdin: HTMLTextAreaElement, sendButton: HTMLButtonElement): ReadableStream {
+  static async *create(
+    stdin: HTMLTextAreaElement,
+    sendButton: HTMLButtonElement
+  ): AsyncGenerator<Uint8Array, never, void> {
     const encoder = new TextEncoder();
-    return new ReadableStream({
-      type: "bytes" as any,
-      async start(controller) {
-        while (true) {
-          await new Promise<void>((resolve) => {
-            sendButton.addEventListener(
-              "click",
-              () => {
-                const payload = stdin.value;
-                const message = `Content-Length: ${payload.length}\r\n\r\n${payload}`;
-                const bytes = encoder.encode(message);
-                controller.enqueue(bytes);
-                stdin.value = "";
-                resolve();
-              },
-              { once: true }
-            );
-          });
-        }
-      },
-    });
+    while (true) {
+      const bytes = await new Promise<Uint8Array>((resolve) => {
+        sendButton.addEventListener(
+          "click",
+          () => {
+            const payload = stdin.value;
+            const message = `Content-Length: ${payload.length}\r\n\r\n${payload}`;
+            stdin.value = "";
+            resolve(encoder.encode(message));
+          },
+          { once: true }
+        );
+      });
+      yield bytes;
+    }
   }
 }
+
+// NOTE: unused ReadableByteStream based implementation. See comments in server/src/lib.rs.
+//
+// class LspStdin {
+//   static create(stdin: HTMLTextAreaElement, sendButton: HTMLButtonElement): ReadableStream {
+//     const encoder = new TextEncoder();
+//     return new ReadableStream({
+//       type: "bytes" as any,
+//       async start(controller) {
+//         while (true) {
+//           await new Promise<void>((resolve) => {
+//             sendButton.addEventListener(
+//               "click",
+//               () => {
+//                 const payload = stdin.value;
+//                 const message = `Content-Length: ${payload.length}\r\n\r\n${payload}`;
+//                 const bytes = encoder.encode(message);
+//                 controller.enqueue(bytes);
+//                 stdin.value = "";
+//                 resolve();
+//               },
+//               { once: true }
+//             );
+//           });
+//         }
+//       },
+//     });
+//   }
+// }
 
 class LspStdout {
   static create(stdout: HTMLTextAreaElement): WritableStream {
