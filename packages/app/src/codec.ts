eff846067505de23d5eacaf08be1c40f262e3a1b
silvanshade <silvanshade@users.noreply.github.com>
2022-06-16 12:27:24 -0600

    Delete resolved promises in the map after use

--- packages/app/src/codec.ts
+++ packages/app/src/codec.ts
@@ -29,15 +29,23 @@ export class Headers {
 export class PromiseMap<K, V extends { toString(): string }> {
   #map: Map<K, PromiseMap.type<V>> = new Map();
 
-  async get(key: K & { toString(): string }): Promise<V> {
+  get(key: K & { toString(): string }): null | Promise<V> {
     let initialized: PromiseMap.type<V>;
+    // if the entry doesn't exist, set it
     if (!this.#map.has(key)) {
       initialized = this.#set(key);
     } else {
+      // otherwise return the entry
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       initialized = this.#map.get(key)!;
     }
-    return initialized.promise;
+    // if the entry is a pending promise, return it
+    if (initialized.status === "pending") {
+      return initialized.promise;
+    } else {
+      // otherwise return null
+      return null;
+    }
   }
 
   #set(key: K, value?: V): PromiseMap.type<V> {
@@ -45,26 +53,33 @@ export class PromiseMap<K, V extends { toString(): string }> {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       return this.#map.get(key)!;
     }
-    // eslint-disable-next-line prefer-const
-    let partial: PromiseMap.partial<V> = {};
-    partial.promise = new Promise<V>((resolve) => {
-      partial.resolve = resolve;
+    // placeholder resolver for entry
+    let resolve = (item: V) => {
+      void item;
+    };
+    // promise for entry (which assigns the resolver
+    const promise = new Promise<V>((resolver) => {
+      resolve = resolver;
     });
-    const initialized = partial as PromiseMap.type<V>;
+    // the initialized entry
+    const initialized: PromiseMap.type<V> = { status: "pending", resolve, promise };
     if (null != value) {
       initialized.resolve(value);
     }
+    // set the entry
     this.#map.set(key, initialized);
     return initialized;
   }
 
-  has(key: K): boolean {
-    return this.#map.has(key);
-  }
-
   set(key: K & { toString(): string }, value: V): this {
     const initialized = this.#set(key, value);
-    initialized.resolve(value);
+    // if the promise is pending ...
+    if (initialized.status === "pending") {
+      // ... set the entry status to resolved to free the promise
+      this.#map.set(key, { status: "resolved" });
+      // ... and resolve the promise with the given value
+      initialized.resolve(value);
+    }
     return this;
   }
 
@@ -75,8 +90,7 @@ export class PromiseMap<K, V extends { toString(): string }> {
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace PromiseMap {
-  export type type<V> = { resolve: (item: V) => void; promise: Promise<V> };
-  export type partial<V> = { resolve?: (item: V) => void; promise?: Promise<V> };
+  export type type<V> = { status: "pending"; resolve: (item: V) => void; promise: Promise<V> } | { status: "resolved" };
 }
 
 // FIXME: tracing effiency
@@ -90,7 +104,7 @@ export class IntoServer extends Queue<Uint8Array> implements AsyncGenerator<Uint
 }
 
 export interface FromServer extends WritableStream<Uint8Array> {
-  readonly responses: { get(key: number | string): Promise<vsrpc.ResponseMessage> };
+  readonly responses: { get(key: number | string): null | Promise<vsrpc.ResponseMessage> };
   readonly notifications: AsyncGenerator<vsrpc.NotificationMessage, never, void>;
   readonly requests: AsyncGenerator<vsrpc.RequestMessage, never, void>;
 }
